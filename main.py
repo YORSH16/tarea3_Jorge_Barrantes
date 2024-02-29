ec2e292b5972d32308cef141634fc6ef02d0c4cf
YORSH16 <109051056+YORSH16@users.noreply.github.com>
2024-02-28 20:01:47 -0600

    Una funcion para multiplicar

--- main.py
+++ main.py
@@ -14,5 +14,11 @@ if __name__ == "__main__":
     resultado = resta(num1, num2)
     print("El resultado es...", resultado)
 
-
+def multiplicacion(a,b):
+    return a - b
+if __name__ == "__main__":
+    num1 = int(input("Ingrese el primer numero"))
+    num2 = int(input("Ingrese el segundo"))
+    resultado = resta(num1, num2)
+    print("El resultado es...", resultado)
 
