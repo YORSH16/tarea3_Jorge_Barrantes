c32aff6b2012dd1f11907eefe55789ab069873e2
YORSH16 <109051056+YORSH16@users.noreply.github.com>
2024-02-28 18:40:23 -0600

    Una funcion de suma

---
+++ main.py
@@ -0,0 +1,8 @@
+def suma(a, b):
+    return a + b
+if __name__ == "__main__":
+    num1 = int(input("Ingrese un número: "))
+    num2 = int(input("Ingrese el segundo número: "))
+    resultado = suma(num1, num2)
+    print("El resultado es...", resultado)
+#%%
