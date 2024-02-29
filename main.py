8f3defb405b81129c90f9da5290415ffbfea69f8
YORSH16 <109051056+YORSH16@users.noreply.github.com>
2024-02-28 18:59:23 -0600

    Una funcion de resta

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ if __name__ == "__main__":
     resultado = suma(num1, num2)
     print("El resultado es...", resultado)
 #%%
+def resta(a,b):
+    return a - b
+if __name__ == "__main__":
+    num1 = int(input("Ingrese el primer numero"))
+    num2 = int(input("Ingrese el segundo"))
+    resultado = resta(num1, num2)
+    print("El resultado es...", resultado)
+
+
+
